2605556f5b830fb424826063a85ed0d0ff4de04d
elroyg1 <elroy.galbraith@gmail.com>
2018-07-26 19:53:22 -0500

    added three tabs

--- ICHSDetentionReport/app.R
+++ ICHSDetentionReport/app.R
@@ -4,24 +4,9 @@ library(googlesheets)
 library(dplyr)
 library(DT)
 
-### Get Data
-detentionserved <- gs_key("1ltL1QjCUrgK3CBHKhzKHsOHNDce3Zj_1Lykhqtifauk") %>%
-  gs_read(ws = 1)
-
-detentionissued <- gs_key("1ltL1QjCUrgK3CBHKhzKHsOHNDce3Zj_1Lykhqtifauk") %>%
-  gs_read(ws = 2)
-
-report_table <- detentionissued %>%
-  left_join(detentionserved, by = c("First Name",
-                                    "Last Name",
-                                    "Grade",
-                                    c("Date given" = "Date Given"))) %>%
-  select(2,4,5,8,9,12,13)
-###
-
 # Define UI for application
 ui <- navbarPage(
-  
+  # Theme
   theme = shinytheme("cerulean"),
    
    # Application title
@@ -29,23 +14,19 @@ ui <- navbarPage(
    
    # Tab for issuance
    tabPanel(
-     "Detentions Issued",
-     sidebarLayout(
-       
-       sidebarPanel(
-         htmlOutput(
-           "issue",
-           container = tags$iframe,
-           src = "https://docs.google.com/forms/d/e/1FAIpQLSfYQnGM67fs34TTBeU5XEOuxfZx0_iQ9cEWaIyrmiHTXexppA/viewform?embedded=true",
-           width = 400,
-           height = 625,
-           frameborder = 0,
-           marginheight = 0)
-         ),
      
-       mainPanel(
-         DT::dataTableOutput("issued"),
-         actionButton("refresh", "Refresh")
+     "Detentions Issued",
+     
+     wellPanel(
+       
+       htmlOutput(
+         "issue",
+         container = tags$iframe,
+         src = "https://docs.google.com/forms/d/e/1FAIpQLSfYQnGM67fs34TTBeU5XEOuxfZx0_iQ9cEWaIyrmiHTXexppA/viewform?embedded=true",
+         width = 800,
+         height = 525,
+         frameborder = 0,
+         marginheight = 0
        )
      )
    ),
@@ -54,61 +35,79 @@ ui <- navbarPage(
    tabPanel(
      "Detentions Served",
      
-     sidebarLayout(
-       
-       sidebarPanel(
-         htmlOutput(
+     wellPanel(
+       htmlOutput(
          "serve",
          container = tags$iframe,
          src = "https://docs.google.com/forms/d/e/1FAIpQLSc_L7aNSBdJ4UE7DpsX2NNdIKkOYt9qTg1KiCk4lWzHkiWvWw/viewform?embedded=true",
-         width = 400,
-         height = 625,
+         width = 800,
+         height = 425,
          frameborder = 0,
          marginheight = 0
        )
-       ),
-       mainPanel(
-         DT::dataTableOutput("served"),
-         actionButton("refresh", "Refresh")
-         )
      )
-   )
+     ),
+  # Tab for report 
+  tabPanel(   
+    "Report",
+  
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons(
+        "choice",
+        "Choose one",
+        choices = c(
+          "Detentions Issued" = 1,
+          "Detentions Served" = 2
+        )
+      ),
+      actionButton("display","Display")
+    ),
+    mainPanel(
+      fluidRow(DT::dataTableOutput("report"),
+               actionButton("refresh", "Refresh"))
+    )
+  )
+)
 )
 
 # Define server logic
 server <- function(input, output) {
-   
-   output$served <- DT::renderDataTable({
-     
-     input$refresh
+  
+  renderText(input$date)
+  
+  observeEvent(input$display,{
+    
+    ws <- reactive({
+      ifelse(
+        input$choice == 1,
+        2,
+        1
+      )
+    })
 
-     report_table %>%
-       DT::datatable(
-         extensions = "Buttons",
-         options = list(
-           dom = "Bfrtip",
-           buttons = c("excel", "pdf","print")
-         )
-       )
-
-   })
-   
-   output$issued <- DT::renderDataTable({
-     
-     input$refresh
-
-     detentionissued %>%
-       select(-Timestamp) %>%
-       DT::datatable(
-         extensions = "Buttons",
-         options = list(
-           dom = "Bfrtip",
-           buttons = c("excel","pdf","print")
-         )
-       )
-   })
-
-   }
+    input$refresh
+    
+    output$report <- renderDataTable({
+      
+      "1ltL1QjCUrgK3CBHKhzKHsOHNDce3Zj_1Lykhqtifauk" %>%
+        gs_key() %>%
+        gs_read(ws=ws()) %>% 
+        select(-Timestamp) %>%
+        datatable(
+          rownames = F,
+          extensions = "Buttons",
+          options = list(
+            dom = "Bfrtip",
+            buttons = c("excel", "pdf","print")
+          )
+        )
+      
+    })
+    
+  })
+  
+}
 
 # Run the application 
 shinyApp(ui = ui, server = server)
